f2cf81f225663d51db6d4c97cf852234374eb28f
MKS-Student <mks.student@hackreactor.com>
2015-03-27 15:25:37 -0500

    basic server runs

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,5 +3,24 @@ var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // res.end(archive.paths.list);
+
+switch(req.method){
+  case 'GET':
+    res.writeHead(200, defaultCorsHeaders);
+    res.end('Hello, World!');
+  break;
+  
+  case 'POST':
+  break;
+}
+
+};
+
+var defaultCorsHeaders = {
+  "Content-Type":"text/html",
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
 };
